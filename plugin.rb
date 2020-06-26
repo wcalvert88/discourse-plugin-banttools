22318b71a6d91d86daa3e13d71ad896911424bb2
Wade Calvert <wcalvert88@gmail.com>
2020-06-26 17:50:34 -0400

    got banner working

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: banttools
+# about: Create a site banner, add hot topics to homepage layout, and other features as needed for client
+# version: 0.0.1
+# authors: Wade Calvert
+
+enabled_site_setting :banttools_banner_link
